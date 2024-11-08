b36f70bb17ded077d54ee1f7c93257142f44a09a
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-11-07 20:43:22 -0500

    Update c_build_script.py

--- c_build_script.py
+++ c_build_script.py
@@ -4,14 +4,12 @@ from c_build.source.Manager import *
 # --------------------------------------------------------------------------------------s
 
 compiler_name = C_BUILD_COMPILER_NAME() if C_BUILD_IS_DEPENDENCY() else "cl"
-compiler_std_version = "c11"
 compiler_warning_level = ""
 compiler_disable_specific_warnings = []
 compiler_treat_warnings_as_errors = True
 
 project_name = "ckit_graphics"
 project_dependencies = ["ckit"]
-project_rebuild_project_dependencies = False
 project_debug_with_visual_studio = True
 project_executable_procedures = ["ckit_graphics_test.exe", "ckit_pong.exe"]
 
