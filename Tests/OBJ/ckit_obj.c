04ece6ae1fe60fbf5f4eb5b35b908b394eccd514
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-16 23:01:16 -0400

    Update ckit_obj.c

--- Tests/OBJ/ckit_obj.c
+++ Tests/OBJ/ckit_obj.c
@@ -5,7 +5,7 @@ int main(int argc, char** argv) {
 	ckit_init();
 
 	u32 width = 1200;
-	u32 height = 800;
+	u32 height = 920;
 
 	CKIT_Window* window = ckit_window_create(width, height, "CKIT OBJ Model");
 	CKSGL graphics = cksgl_create(&window->bitmap.memory, (u16*)&window->bitmap.width, (u16*)&window->bitmap.height);
@@ -29,25 +29,23 @@ int main(int argc, char** argv) {
 		{ // RENDER
 			cksgl_clear_color(graphics, CKIT_COLOR_BLACK);
 
-			for (int i=0; i < ckit_vector_count(model.faces) - 3; i++) {
-				for (int j = 0; j < 3; j++) {
-					u32 f_index0 = model.faces[i + j][0];
-					u32 f_index1 = model.faces[i + ((j + 1) % 3)][0];
+			for (int i = 0; i < ckit_vector_count(model.faces) - 1; i++) {
+				u32 f_index0 = model.faces[i][0];
+				u32 f_index1 = model.faces[i + 1][0];
 
-					CKIT_Vector3 v0 = model.verts[f_index0];
-					CKIT_Vector3 v1 = model.verts[f_index1];
+				CKIT_Vector3 v0 = model.verts[f_index0];
+				CKIT_Vector3 v1 = model.verts[f_index1];
 
-					u32 y_offset = 0;
+				u32 y_offset = 0;
 
-					int x0 = (int)((v0.x + 1.0) * width / 2.0);
-					int y0 = (int)((v0.y + 1.0) * height / 2.0) + y_offset;
-					int x1 = (int)((v1.x + 1.0) * width / 2.0);
-					int y1 = (int)((v1.y + 1.0) * height / 2.0) + y_offset;
+				int x0 = (int)((v0.x + 1.0) * width / 2.0);
+				int y0 = (int)((v0.y + 1.0) * height / 2.0) + y_offset;
+				int x1 = (int)((v1.x + 1.0) * width / 2.0);
+				int y1 = (int)((v1.y + 1.0) * height / 2.0) + y_offset;
 
-					CKIT_Vector3 p0 = {x0, y0, 0};
-					CKIT_Vector3 p1 = {x1, y1, 0};
-					cksgl_draw_line(graphics, p0, p1, CKIT_COLOR_WHITE);
-				}
+				CKIT_Vector3 p0 = {x0, y0, 0};
+				CKIT_Vector3 p1 = {x1, y1, 0};
+				cksgl_draw_line(graphics, p0, p1, CKIT_COLOR_WHITE);
 			}
 
 			cksgl_flip_vertically(graphics);
