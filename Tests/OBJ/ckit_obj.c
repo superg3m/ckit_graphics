2809d395cb8eb638de83c91394d1b7f953c012e3
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-03-17 09:50:19 -0400

    interesting???

--- Tests/OBJ/ckit_obj.c
+++ Tests/OBJ/ckit_obj.c
@@ -1,6 +1,14 @@
 #include "../../ckit_graphics.h"
 #include "./obj_model.c"
 
+CKIT_Vector3 project_to_screen(CKIT_Vector3 point, int width, int height, int y_offset) {
+    CKIT_Vector3 screen_pos;
+    screen_pos.x = (int)((point.x + 1.0) * width / 2.0);
+    screen_pos.y = (int)((point.y + 1.0) * height / 2.0) + y_offset;
+	screen_pos.z = 0;
+    return screen_pos;
+}
+
 int main(int argc, char** argv) {
 	ckit_init();
 
@@ -29,23 +37,16 @@ int main(int argc, char** argv) {
 		{ // RENDER
 			cksgl_clear_color(graphics, CKIT_COLOR_BLACK);
 
-			for (int i = 0; i < ckit_vector_count(model.faces) - 1; i++) {
+			for (int i = 0; i < ckit_vector_count(model.faces) - 2; i++) {
 				u32 f_index0 = model.faces[i][0];
 				u32 f_index1 = model.faces[i + 1][0];
+				u32 f_index2 = model.faces[i + 2][0];
 
-				CKIT_Vector3 v0 = model.verts[f_index0];
-				CKIT_Vector3 v1 = model.verts[f_index1];
+				CKIT_Vector3 p0 = project_to_screen(model.verts[f_index0], width, height, 0);
+				CKIT_Vector3 p1 = project_to_screen(model.verts[f_index1], width, height, 0);
+				CKIT_Vector3 p2 = project_to_screen(model.verts[f_index2], width, height, 0);
 
-				u32 y_offset = 0;
-
-				int x0 = (int)((v0.x + 1.0) * width / 2.0);
-				int y0 = (int)((v0.y + 1.0) * height / 2.0) + y_offset;
-				int x1 = (int)((v1.x + 1.0) * width / 2.0);
-				int y1 = (int)((v1.y + 1.0) * height / 2.0) + y_offset;
-
-				CKIT_Vector3 p0 = {x0, y0, 0};
-				CKIT_Vector3 p1 = {x1, y1, 0};
-				cksgl_draw_line(graphics, p0, p1, CKIT_COLOR_WHITE);
+				cksgl_draw_triangle(graphics, p0, p1, p2, FALSE, CKIT_COLOR_WHITE);
 			}
 
 			cksgl_flip_vertically(graphics);
